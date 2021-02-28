3bec8c870f536b4dedc7470a3185bf04b215054c
Bruna Lima Leao <lleaobruna@gmail.com>
2021-02-28 04:01:08 -0300

    added integration tests

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,15 @@
-import { createConnection } from "typeorm"
+import { Connection, createConnection, getConnectionOptions } from "typeorm"
+require('dotenv').config()
 
-createConnection()
+export default async (): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions()
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            database: process.env.NODE_ENV === 'test' 
+                ? '.src/database/database.test.sqlite' 
+                : defaultOptions.database
+        })
+    )
+}
+
